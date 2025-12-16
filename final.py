80294858b84504db1402101253894a8ec258b5de
tsuianya-stack <tsuianya@icloud.com>
2025-12-16 13:20:33 +0800

    Update final.py

--- final.py
+++ final.py
@@ -1,1 +1,14 @@
+import streamlit as st
+import pandas as pd
+import numpy as np
 
+data = { "Product": ["Apple", "Banana", "Pear"], 
+        "Price": [5, 10, 15],
+        "Quantity": [10, 9, 12]}
+
+df=pd.DataFrame(data)
+
+filtered_df = df[df["Quantity" >10]]
+st.write(filtered_df)
+total_revenue = df[df["Quantity"]*df["Sales"]]
+st.write(total_revenue)
